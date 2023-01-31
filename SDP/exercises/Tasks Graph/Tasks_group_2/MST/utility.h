076d9a65264a554cfcba7388f4e400ea45a8045c
VikiIvanova <viki.ivanova756971@gmail.com>
2023-01-31 12:44:37 +0200

    MST problem

---
+++ SDP/exercises/Tasks Graph/Tasks_group_2/MST/utility.h
@@ -0,0 +1,11 @@
+#include <vector>
+
+//A structure to represent a subset for union-find
+struct subset
+{
+    int parent;
+    int rank;
+};
+
+int find(std::vector<subset>& subsets, int i);
+void Union(std::vector<subset>& subsets, int x, int y);
